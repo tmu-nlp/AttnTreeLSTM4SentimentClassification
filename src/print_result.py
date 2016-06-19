2deb85dd89c974bb46812afdd05293428cd87365
ryosukee <tmcit.miyazaki@gmail.com>
2016-06-19 19:29:30 +0900

    [add] dict info

--- src/print_result.py
+++ src/print_result.py
@@ -13,28 +13,30 @@ for dir in dirs:
     att = d['attention']
     lr = d['lr']
     comp = d['composition']
+    pold = d['pol_dict']
     dropout = None
     if 'dropout' in d:
         dropout = d['dropout']
     att_tgt = None
     if 'attention_target' in d:
         att_tgt = d['attention_target']
-    results[name] = (test, dev, opt, lr, att, att_tgt, dropout, comp)
+    results[name] = (test, dev, opt, lr, att, att_tgt, pold, dropout, comp)
 keys = list(d.keys())
 print('keys')
 print(keys)
 print()
-
-table = PrettyTable(['test', 'dev', 'optimizer', 'lr', 'attention', 'attention_target', 'dropout', 'composition', 'name'])
+print('sort key = dev')
+table = PrettyTable(['test', 'dev', 'optimizer', 'lr', 'attention', 'attention_target', 'pol_dict', 'dropout', 'composition', 'name'])
 table.align['optimizer'] = 'l'
 table.align['lr'] = 'l'
 table.align['attention'] = 'l'
 table.align['attention_target'] = 'l'
+table.align['pol_dict'] = 'l'
 table.align['dropout'] = 'l'
 table.align['composition'] = 'l'
 table.align['name'] = 'l'
-for name, (test, dev, opt, lr, att, att_tgt, drop, comp) in sorted(results.items(), key=lambda x: -x[1][1]):
+for name, (test, dev, opt, lr, att, att_tgt, pold, drop, comp) in sorted(results.items(), key=lambda x: -x[1][1]):
     dev = '{:.5f}'.format(dev)
     test = '{:.5f}'.format(test)
-    table.add_row([test, dev, opt, lr, att, att_tgt, drop, comp, name])
+    table.add_row([test, dev, opt, lr, att, att_tgt, pold, drop, comp, name])
 print(table)
